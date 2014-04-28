127ec8f26081be99c7bc58bc2f202f9ac5315668
danklingjr <dan@webtek.cc>
2014-04-28 13:59:49 -0400

    home

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$('.hero h2').css({
+	"opacity": "0"
+});
 
+$('.hero h2').delay(500).animate({
+	"opacity": "1",
+	"left": "0px"
+}, 700);
---
+++ js/min/main-ck.js
@@ -0,0 +1,1 @@
+$(".hero h2").css({opacity:"0"}),$(".hero h2").delay(500).animate({opacity:"1",left:"0px"},700);
