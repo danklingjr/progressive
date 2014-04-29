6adc354cb87a8342765e4e240d66e05789c91097
danklingjr <dan@webtek.cc>
2014-04-29 16:40:52 -0400

    home

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-$('.hero h2').css({
-	"opacity": "0"
-});
+$(document).ready(function () {
+	$('.hero h2').css({
+		"opacity": "0"
+	});
 
-$('.hero h2').delay(500).animate({
-	"opacity": "1",
-	"left": "0px"
-}, 700);
+	$('.hero h2').delay(500).animate({
+		"opacity": "1",
+		"left": "0px"
+	}, 700);
+});
--- js/min/main-ck.js
+++ js/min/main-ck.js
@@ -1,1 +1,1 @@
-$(".hero h2").css({opacity:"0"}),$(".hero h2").delay(500).animate({opacity:"1",left:"0px"},700);
+$(document).ready(function(){$(".hero h2").css({opacity:"0"}),$(".hero h2").delay(500).animate({opacity:"1",left:"0px"},700)});
